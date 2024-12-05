8598c10e3c5f2c3871dab0f6a272ce7239e9efa7
Luis Moreno <luismorenogv@gmail.com>
2024-12-05 12:27:41 +0100

    basic LD2410 presence sensor interaction

---
+++ include/RoomNode/LD2410.h
@@ -0,0 +1,30 @@
+/**
+ * @file LD2410.h
+ * @brief Declaration of LD2410 class for managing presence sensor
+ *
+ * @author Luis Moreno
+ * @date Dec 4, 2024
+ */
+
+#pragma once
+
+#include <Arduino.h>
+#include <freertos/queue.h>
+#include "config.h"
+
+class LD2410 {
+public:
+    LD2410(QueueHandle_t queue);
+    ~LD2410();
+
+    // Delete copy constructor and assignment operator to prevent multiple instances
+    LD2410(const LD2410&) = delete;
+    LD2410& operator=(const LD2410&) = delete;
+
+private:
+    static void IRAM_ATTR staticPresenceISR();
+    void IRAM_ATTR presenceISR();
+
+    static LD2410* instance; // Static pointer to the class instance
+    QueueHandle_t presenceQueue;
+};
